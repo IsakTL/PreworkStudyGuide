656e2c26b542b206a7e49f7c2faee9c3b10befa1
Isak <isaktlarsson99@gmail.com>
2024-09-15 23:44:49 -0500

    Add more JS to the project

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,43 @@
-const topics = ["HTML","CSS","Git","JavaScript"];
-const shapes = ["Circle","Square","Sherman","Squidward"];
-for(let i=0;i<topics.length;i++){
-    console.log("Let's study "+topics[i]+"!");
+const topics = ["HTML", "CSS", "Git", "JavaScript"];
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
+const shapes = ["Circle", "Square", "Sherman", "Squidward"];
+
+function listTopics() {
+    for (let i = 0; i < topics.length; i++) {
+        console.log(topics[i])
+    }
 }
+
+function alliterate() {
+    if (randomTopic === 'HTML') {
+        console.log("Let's hack some HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study some CSS!");
+    } else if (randomTopic === 'Git') {
+        console.log("Let's get some Git!");
+    } else if (randomTopic === 'JavaScript') {
+        console.log("Let's jive some JavaScript.");
+    } else {
+        console.log('Huh?');
+    }
+}
+
 /*UNUSED
-if (topic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (topic === 'Git') {
- console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
+function selectTopic() {
+    if (randomTopic === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+        console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log('Huh?');
+    }
 }
 */
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+alliterate();
